e9b2e90a1bd0d3ebcf37825c33a87cf4999a58a7
WhizDev <44204652+WhizDev@users.noreply.github.com>
2019-01-14 09:44:14 +0200

    Delete 32767.bat

--- 32767.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:stresstest
-set FC=%random%
-if exist %FC% echo Error: file (%FC%) already exists - operation skipped!
-if exist %FC% goto stresstest
-if not exist %FC% echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random% > %FC%
-echo File created: %FC%
-GOTO stresstest
